436dc9d278d4e386813aa8bfcc5b41c1bbfde3a4
WakeupSid <83629208+sid414@users.noreply.github.com>
2024-05-06 18:04:40 +0000

    updatedname

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail="siddh@gmail.com"
+var accountPassword ="121212"
+accountCity ="Jaipur"
+
+// accountId=3; not allowed
+accountEmail="sg@gmail.com"
+accountPassword="232323"
+accountCity ="Delhi"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
