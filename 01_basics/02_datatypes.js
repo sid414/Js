21d01ff78bc606068146d01c17dca4d29329b85f
WakeupSid <83629208+sid414@users.noreply.github.com>
2024-05-06 18:05:20 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+// abhi automatically hi newer version mein treat kiya jata hai
+
+// alert(3+3)  we cannot use it as we are using node js ,and not browser
+
+//javascript ka engine browser ke ander chupa hua hai n jb hum developer console kholenge toh vhan jo bhi likh rhe hai voh doc ke form mein hai
+
+console.log(3+3)
+// code readability should be high
+console.log("sid")
+
+
+// primitve datatypes
+
+let name ="sid"  // string
+let age =18;   // number
+let isLoggedIn =true  // bool fn
+let state;    // undefined
+let numb =null;   //null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (represents empty value)
+// undefined => 
+// symbol => unique  
+
+// object
+console.log(typeof null); //object (output)
+console.log(typeof undefined) // undefined (output)
