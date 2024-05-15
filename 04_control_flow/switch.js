9577f15a8fa1f21241bacd86588aac408f9a9b37
WakeupSid <83629208+sid414@users.noreply.github.com>
2024-05-15 18:26:03 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,56 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month =3
+//key ->voh value jo check krni hai
+// short trick -shift +alt+down arrow se selected code duplicate hojata hai
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+    
+//     default:
+//         console.log("Default case match");
+//         break;
+// }
+
+// by deafult kya hota hai , ki agr break na ho aur value match hojaye toh uske neeche ka saara code run hojata hai
+
+// code if the value is string
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
