bfa1a45b6da11dec838a1f9649e3f961cf2d5de3
WakeupSid <83629208+sid414@users.noreply.github.com>
2024-05-16 10:49:18 +0000

    do while loop

---
+++ 05_iterations/02_TWO.js
@@ -0,0 +1,25 @@
+// while (condition) {
+    
+// }
+let index=0
+while(index<=10){
+    // console.log(`Value of index is :${index }`);
+    index =index +2;
+}
+
+let myArray=["flash","spiderman","batman"]
+
+let ind=0;
+while(ind < myArray.length){
+    // console.log(`Value is ${myArray[ind]}`);
+    ind++;
+}
+
+//pehle kaam hoga uske baad condition check hogi
+
+// let score=  11 // do while loop will print 11 coz condition baad mein check hui
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <=10);
